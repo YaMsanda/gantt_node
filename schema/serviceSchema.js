7234864c82788c0a4840e3d022fd031e91f0d380
YaMsanda <lolo.mess.17@gmail.com>
2020-01-19 19:17:10 +0100

    setting sockets route and editing mongoose model to fit the need

--- schema/serviceSchema.js
+++ schema/serviceSchema.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const serviceSchema = new Schema({
-    nameService: {type: String, required:true, unique: true, trim: true}
+    nameService: { type: String, required: true, unique: true, trim: true },
+    serviceData: { type: String, required: true, unique: false, trim: false }
 });
 
 module.exports = mongoose.model('serviceSchema', serviceSchema);
--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const { join } = require('path');
+const serviceModel = require('./schema/serviceSchema.js')
 
-let client = socket.connect('http://51.15.137.122:18000/', { reconnect: true });
-client.on('connect', () => {
-    console.log('connected')
-    client.emit('needHelp');
-});
 
 
 mongoose.connect('mongodb://localhost:27017/nodegantt', { useNewUrlParser: true }, () => {
@@ -18,9 +14,47 @@ mongoose.connect('mongodb://localhost:27017/nodegantt', { useNewUrlParser: true 
         process.exit(1);
     }
 });
+var db = mongoose.connection;
+
+function getGanttFromMongoose() {
+    serviceModel.find({ 'nameService': 'base' }, 'nameService serviceData', function(err, services) {
+        if (err) {
+            return handleError(err);
+        } else {
+            //need to parse services and get it in the right format for client gantt
+            return services;
+        }
+    })
+}
+
+function setGanttOnMongoose(data) {
+    //need to parse data and get it in the right format for database
+    var ganttToInsert = new serviceModel({ nameService: 'base', serviceData: data });
+    ganttToInsert.save(function(err, services) {
+        if (err) return handleError(err);
+    })
+}
 
 const service = require(join(__dirname, 'controller', 'serviceController.js'));
 
 app.use('/service', service.router);
 
 app.listen(port, () => console.log('!! server online on ' + port));
+
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    socket.on('getCurrentGantt', (data) => {
+        gantt = getGanttFromMongoose();
+        io.sockets.emit('currentGantt', { gantt: gantt });
+    })
+    socket.on('setCurrentGantt', (data) => {
+        setGanttOnMongoose(data);
+    }))
+})
+
+let client = socket.connect('http://51.15.137.122:18000/', { reconnect: true });
+client.on('connect', () => {
+    console.log('connected')
+    client.emit('needHelp');
+});
